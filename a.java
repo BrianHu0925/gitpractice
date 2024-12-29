9ab5cb9a0fa3eddf293e3545df0f48dc7243cfd4
BrianHu0925 <j100376@my.cmsh.cyc.edu.tw>
2024-12-29 15:41:11 +0800

    [add] hello world

---
+++ a.java
@@ -0,0 +1,11 @@
+public class a {
+    /**
+     * Main method to print a message to the console.
+     * @param args String array of command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    
+    
+}
